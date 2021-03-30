2fc67f979d7e20b3c2b398f83c407967cc199c55
Jouni Mäkelä <jouni.img@gmail.com>
2021-03-30 14:28:20 +0300

    Decrypt claims from identity token

--- lib/crisp/identity_service_broker.ex
+++ lib/crisp/identity_service_broker.ex
@@ -48,9 +48,34 @@ defmodule Crisp.IdentityServiceBroker do
          {"client_assertion", token}
        ]}
 
+    require IEx
+
     case HTTPoison.post(url, body, headers) do
       {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
-        {:ok, Jason.decode!(body)}
+        signer = Joken.Signer.create("RS256", %{"pem" => decrypt_pem()})
+        decoded_body = Jason.decode!(body)
+        id_token = decoded_body["id_token"]
+
+        {decrypted_token, _jwe} = JOSE.JWE.block_decrypt(signer.jwk, id_token)
+        Joken.expand(decrypted_token)
+
+        # JWKS
+        {:ok, %HTTPoison.Response{status_code: 200, body: body}} =
+          HTTPoison.get("https://isb-test.op.fi/jwks/broker")
+
+        jwks_response = Jason.decode!(body)
+        jwk = jwks_response["keys"] |> hd |> JOSE.JWK.from()
+
+        # Verify
+        {true, %JOSE.JWT{fields: claims}} =
+          JOSE.JWT.verify_strict(jwk, ["RS256"], decrypted_token)
+
+        # TODO: Should validate
+
+        result =
+          Crisp.IdentityServiceBroker.Token.verify_and_validate(decoded_body["id_token"], signer)
+
+        IEx.pry()
 
       _ ->
         :error
@@ -69,5 +94,41 @@ defmodule Crisp.IdentityServiceBroker do
        personal_identity_code: "010100-969P",
        nonce: encoded_nonce
      }}
+  end
+
+  def decrypt_pem() do
+    """
+    -----BEGIN RSA PRIVATE KEY-----
+    MIIEpAIBAAKCAQEAxRXWHYRvsFJ6WGSiLSDZ5KgRHglSpTFbZsrZ/P6Sa9ZKeStO
+    hcP0M4FO9ORdc12MsTPlFMIQy+6TiJXvZ8pxwcweFzaeGVBWtI/72waAHu5SSFnD
+    pJ9SVRYCdCU95ONZAzNMaNNHTPivg5KgYL40yXZqGSCIApAEp7RcE6hm6PYdXLeW
+    f/ATKNfVh9WMpMg49B5HWI7JPVjN8xVi73wjMKgKcReuX8T17HuF7wS0LZwWr80R
+    8sXCevMKdUhah6YcF654eDsqYCEVrVAVOpdSMsmwkuoN0mnDmu8ltyCi+/46ibfm
+    gDWFv/FIx+qAx92ADtBFYyhAiWEia4a67J7h6QIDAQABAoIBAEJd4RVdhJLWo1Zi
+    xO33cwh5xBHeR9NS9Jl/07pLK5letwNonu+SlghGEgWLZSC1Lux5Mjo0Q524wDut
+    acoEtRvylWtxu/mAQUJafhA6dyhzTogImkY0VHBMkhT0eYw7Nr1KJ//grdDwqYHf
+    /Vu2kWYKFx5RAxwBzV9/78W63rSxnLkJJ37bD49zoGPPD/p07NyldeYOgB4D+tOj
+    x9cPQYuJyyIicai+U1g32vQoW42LW7WZ8L+9DSX8Za2Y9EeIzzDbnSwSFHC5dbmf
+    LLU2OATVOOC8+CKff5lLUZpU7Huta0MO/vsMbVk4BbX2tRaQjIO5QaZookaEw2nr
+    ft7NgAECgYEA6cu3fgU7qGQ00foUXQ81MQqJnZzQfRybH8mk5K3AxyaPt0xm3bct
+    tr42j5dvSgOeLUltIhyKK7ynu0eewOiOj1EQSr9MUCTX82u/GC5HIoajlpqNOjML
+    3At2tNWp59kK8Xavh/7tziyaxC6jfGs90i1JMLyvw2qqfgD80+G30AECgYEA182T
+    sAhwQDCzNZRTmnLqKpEIyLvvmGRMUMZbYCsTdFIRqJnp1NUWqPRlDrOyC/wjXXhE
+    F8SIlEihFV03EDPQd+lN2S91bfWp6zmQQ/RNV2/EpEnveZ98sDUtWv7gD5y6Y2Og
+    WgbiTjGdi6PdBAujCtQV/+zMTWJgLUDTgb+CkekCgYEAq3dmJv2tiE2d05Ym0y1x
+    JHuG/FpjKi8Nsh2QWsr4BNdKNfPJybgrxcfxehRlYZIPs0UTzh3CVFtzZM4dJ3wn
+    +gJJAZkVXf33h6axKwndz/uRBMZq55FClGqv7+Z4SvsKpJcYG5SjUj+ybkmKS5jX
+    aUsfT6hC+/5B9lHBG6CC0AECgYBSWce/CClVxX/NtXGF4c5MOhZhC56tx7B7roYW
+    F7oPzHWiokFiD4LbLJAX4SjTe/8+3vMVItF/zItz7KNjknI+IBCuDXdBY9rYwZm0
+    5NhSwnsGupQhFGRvlYteSx7TtlpGLTF2yWO4tSRKslhX/9zS+AYBKKr56M2M100f
+    GSbJaQKBgQC6G4U9Voj1JEdBn4DVCi8AvIxOBt6aXCzdXSwCRzrqJxDvVrk/36Ih
+    WZJAdYnb6gmG04R0HMxfhuiYfscC+Fa9/9lhoS930cKLYtzEwf1ngS42+oS52imm
+    /6a/W8zWAlpCfNJBBpwdloX7jfGNKmjwo82gFEbvwP4B5WujraxwMA==
+    -----END RSA PRIVATE KEY-----
+    """
+  end
+
+  def handle_identity_token() do
+    signer = Joken.Signer.create("RS256", %{"pem" => decrypt_pem()})
   end
 end
