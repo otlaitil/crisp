d31f9ab5ab7403e3ccb66513f83067311f88ce4b
Jouni Mäkelä <jouni.img@gmail.com>
2021-03-31 15:54:30 +0300

    Remove IdentityServiceBroker.Token

--- lib/crisp/identity_service_broker/token.ex
+++
@@ -1,9 +0,0 @@
-defmodule Crisp.IdentityServiceBroker.Token do
-  use Joken.Config
-
-  def token_config do
-    default_claims()
-    |> add_claim("iss", fn -> "saippuakauppias" end, &(&1 == "saippuakauppias"))
-    |> add_claim("client_id", fn -> "saippuakauppias" end, &(&1 == "saippuakauppias"))
-  end
-end
