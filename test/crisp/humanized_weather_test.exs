2b1c1d1338e5097413490e0e32583aa2cebb36a2
Jouni Mäkelä <jouni.img@gmail.com>
2021-04-26 11:32:06 +0300

    Protect real implementation with hammox

--- test/crisp/humanized_weather_test.exs
+++ test/crisp/humanized_weather_test.exs
@@ -14,4 +14,9 @@ defmodule Crisp.HumanizedWeatherTest do
     assert Crisp.HumanizedWeather.temp({50.06, 19.94}) == "Current temperature is 30 degrees"
     assert Crisp.HumanizedWeather.humidity({50.06, 19.94}) == "Current humidity is 60%"
   end
+
+  test "real WeatherAPI" do
+    temp = Hammox.protect({Crisp.WeatherAPI, :temp, 1}, Crisp.WeatherAPI.Interface)
+    assert temp.({50.06, 19.94}) == {:ok , 1}
+  end
 end
