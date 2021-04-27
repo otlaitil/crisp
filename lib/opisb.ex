61625bf1bba05db5adf7ae1aab22d09d8749f189
Jouni Mäkelä <jouni.img@gmail.com>
2021-04-27 12:16:34 +0300

    Alias OPISB

--- lib/opisb.ex
+++ lib/opisb.ex
@@ -2,6 +2,8 @@ defmodule OPISB do
   @moduledoc """
   """
 
+  alias OPISB.{GetEmbeddedUi, Initiate, GetIdentity}
+
   @base_url Application.get_env(:opisb, :base_url)
   @client_id Application.get_env(:opisb, :client_id)
   @redirect_uri Application.get_env(:opisb, :redirect_uri)
@@ -9,13 +11,13 @@ defmodule OPISB do
   @decrypt_key Application.get_env(:opisb, :decrypt_key)
 
   def get_embedded_ui() do
-    url = OPISB.GetEmbeddedUi.build_url(@base_url, @client_id)
+    url = GetEmbeddedUi.build_url(@base_url, @client_id)
 
     case HTTPoison.get(url) do
       {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
         body
         |> Jason.decode!()
-        |> OPISB.GetEmbeddedUi.map_to_struct()
+        |> GetEmbeddedUi.map_to_struct()
 
       error ->
         IO.inspect(error, label: "OPISB.get_embedded_ui/0")
@@ -23,24 +25,24 @@ defmodule OPISB do
   end
 
   def initiate_identification(idp, opts \\ []) when is_binary(idp) do
-    OPISB.Initiate.claims(idp, @client_id, @redirect_uri, opts)
-    |> OPISB.Initiate.sign(@signing_key)
-    |> OPISB.Initiate.build_url(@base_url)
+    Initiate.claims(idp, @client_id, @redirect_uri, opts)
+    |> Initiate.sign(@signing_key)
+    |> Initiate.build_url(@base_url)
   end
 
   def get_identity(authorization_code) do
-    {url, body, headers} =
-      OPISB.GetIdentity.claims(@client_id, @redirect_uri)
-      |> OPISB.GetIdentity.sign(@signing_key)
-      |> OPISB.GetIdentity.build_request(@base_url, authorization_code)
+    request =
+      GetIdentity.claims(@client_id, @redirect_uri)
+      |> GetIdentity.sign(@signing_key)
+      |> GetIdentity.build_request(@base_url, authorization_code)
 
-    case HTTPoison.post(url, body, headers) do
+    case HTTPoison.request(request) do
       {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
         %{"id_token" => id_token} = Jason.decode!(body)
-        decrypted_token = OPISB.GetIdentity.decrypt(id_token, @decrypt_key)
-        jwk = OPISB.GetIdentity.jwks()
-        claims = OPISB.GetIdentity.verify(jwk, decrypted_token)
-        OPISB.GetIdentity.validate(claims)
+        decrypted_token = GetIdentity.decrypt(id_token, @decrypt_key)
+        jwk = GetIdentity.jwks()
+        claims = GetIdentity.verify(jwk, decrypted_token)
+        GetIdentity.validate(claims)
 
       _ ->
         :error
