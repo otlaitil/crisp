1e59600e7004a06b52ae2220787e6443962fb702
Jouni Mäkelä <jouni.img@gmail.com>
2021-04-27 17:13:43 +0300

    MockServer /oauth/authorize draft

--- lib/opisb/mock_server.ex
+++ lib/opisb/mock_server.ex
@@ -1,6 +1,7 @@
 defmodule OPISB.MockServer do
   @client_id Application.get_env(:opisb, :client_id)
 
+  import Plug.Conn
   use Plug.Router
 
   plug :match
@@ -32,6 +33,32 @@ defmodule OPISB.MockServer do
     conn
     |> put_resp_content_type("application/json")
     |> send_resp(200, body)
+  end
+
+  get "/oauth/authorize" do
+    conn = fetch_query_params(conn)
+    IO.inspect(conn, label: "MockServer authorize")
+
+    # TODO: If request is not found, return 400/bad_request
+    request = conn.query_params["request"]
+    token = JOSE.JWT.peek_payload(request)
+
+    # 1. Generate authorization_code
+    authorization_code =
+      :crypto.strong_rand_bytes(24)
+      |> Base.url_encode64(padding: false)
+
+    # 2. Generate identity token
+    jwk = JOSE.JWK.from_pem_file("priv/opisb/sandbox-sp-encryption-key.pem")
+    jwt = JOSE.JWT.from(%{"test" => true})
+
+    identity_token =
+      JOSE.JWT.encrypt(jwk, jwt)
+      |> JOSE.JWE.compact()
+      |> elem(1)
+
+    # 3. Save authorization_code and identity_token to ets
+    # 4. Redirect client to token.redirect_uri with authorization_code and state
   end
 
   match _ do
